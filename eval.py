d5130811806d00d644bbbddcc8227fe514c4c60a
Prakhar-Suraj <prakharsurajpython@gmail.com>
2025-03-30 18:16:14 +0530

    Assignment-3

---
+++ eval.py
@@ -0,0 +1,11 @@
+#1
+exp=input("Enter Expression")
+print("Result",eval(exp))
+
+#2
+lst=input("Enter expression")
+print("List:",eval(lst))
+
+#3
+d=input("Enter Dictionary")
+print("Dict",eval(d))
